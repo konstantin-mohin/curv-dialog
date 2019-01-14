85368e29315ec00ea6afc084702ed7e3d8a4ebe9
KonstantiN <konstantin.mohin@gmail.com>
2019-01-14 14:24:58 +0200

    rewrite collapsible according to new super smart component

--- src/components/Approvers/Item/collapsible.js
+++ src/components/Approvers/Item/collapsible.js
@@ -3,6 +3,7 @@ import {LayoutAnimation} from 'react-native';
 
 import Avatar from '~components/Avatar';
 import ListItem from '~components/ListItem';
+import Collapsible from '~components/Collapsible';
 import Wrapper from '~components/Wrapper';
 import ApprovalItem from '~components/Approvers/Item';
 import {
@@ -15,72 +16,62 @@ import { APPROVAL_ITEM_HEIGHT } from '~utils/constants';
 import styles from './styles';
 
 
-export default class CollapsibleItem extends React.Component {
-	state = {
-		collapsed: true
-	}
+const CollapsibleItem = ({data}) => {
 
-	toggleCollapse = () => {
-		LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
-		this.setState(state => (
-			{
-				collapsed: !state.collapsed
-			}
-			));
+	const decision = getUserTransactionStatus(data.members);
+	const avatarIcon = "group";
+	const listHeight = APPROVAL_ITEM_HEIGHT * data.members.length;
 
-	}
-
-	render() {
-		const { data }         = this.props;
-		const { collapsed }    = this.state;
-		const decision         = getUserTransactionStatus(data.members);
-		const rightElement     = collapsed ? 'keyboard-arrow-down' : 'keyboard-arrow-up';
-		const avatarIcon       = "group";
-		const nestedListHeight = collapsed ? 0 : APPROVAL_ITEM_HEIGHT * data.members.length;
-
-		return (
-			<Wrapper backgroundColor="white" overflow="hidden" >
-				<ListItem
-					numberOfLines={2}
-					divider={collapsed}
-					leftElement={
-						<Avatar
-							iconProps={{
-								icon: {
-									name: getUserTransactionBadge(decision),
-									color: "white",
-									size: 23
-								},
-								size: 26,
-								style: {
-									container: {
-										...styles.badgeContainer,
-										backgroundColor: transactionStatusToColor(decision)
+	return (
+		<Collapsible from={APPROVAL_ITEM_HEIGHT} to={listHeight}>
+			{ (toggle, collapsed) =>
+				<Wrapper backgroundColor="white">
+					<ListItem
+						numberOfLines={2}
+						divider={collapsed}
+						leftElement={
+							<Avatar
+								iconProps={{
+									icon: {
+										name: getUserTransactionBadge(decision),
+										color: "white",
+										size: 23
+									},
+									size: 26,
+									style: {
+										container: {
+											...styles.badgeContainer,
+											backgroundColor: transactionStatusToColor(decision)
+										}
 									}
-								}
-							}}
-							icon={avatarIcon}
-							style={{ container: { backgroundColor: getRandomHex() } }}
-						/>
-					}
-					centerElement={{
-						primaryText: data.name,
-						secondaryText: decision,
-					}}
-					style={{ centerElementContainer: styles.centerElementContainer }}
-					rightElement={rightElement}
-					onPress={this.toggleCollapse}
-				/>
-				{
-
-					<Wrapper paddingLeft={30} height={nestedListHeight}>
-						{
-							data.members.map((el, i) => <ApprovalItem key={el.id} data={el}  divider={false} />)
+								}}
+								icon={avatarIcon}
+								style={{ container: { backgroundColor: getRandomHex() } }}
+							/>
 						}
-					</Wrapper>
-				}
-			</Wrapper>
-		);
-	}
-};
+						centerElement={{
+							primaryText: data.name,
+							secondaryText: decision,
+						}}
+						style={{ centerElementContainer: styles.centerElementContainer }}
+						rightElement={collapsed ? 'keyboard-arrow-down' : 'keyboard-arrow-up'}
+						onPress={toggle}
+					/>
+					{
+
+						<Wrapper paddingLeft={30}>
+							{
+								data.members.map((el, i) => <ApprovalItem key={el.id} data={el} divider={false} />)
+							}
+						</Wrapper>
+					}
+				</Wrapper>
+
+
+			}
+		</Collapsible>
+	);
+}
+
+export default CollapsibleItem;
 
